8bcee2822561dacaa1eeb874d68455fbf4dde053
Rogério <rades@live.com>
2022-04-11 17:22:49 -0300

    API.Domain created

---
+++ API.Domain/Cliente.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Domain
+{
+    public class Cliente
+    {
+        [Key]
+        public Guid Id { get; set; }    
+
+        [Required(ErrorMessage ="O Campo {0} é obrigatório")]
+        [MaxLength(50, ErrorMessage ="O Campo {0} deve ter no máximo {1} caracteres")]
+        public string Name { get; set; }
+        public string CPF { get; set; }
+        public int Idade { get; set; }
+    }
+}
